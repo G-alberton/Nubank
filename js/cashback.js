b54e1a09c3becb5a60cb750381be139022a5c8ff
Galberton <gustavo.borghezan030706@gmail.com>
2026-04-07 21:02:06 -0300

    criando a pagina do cashback

---
+++ js/cashback.js
@@ -0,0 +1,34 @@
+const debounce = (func, wait, immediate) => {
+    let timeout;
+    return function(...args){
+        const context = this;
+        const later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll() {
+    const windowTop = window.pageXOffset + (window.innerHeight * 0.85);
+    target.forEach((element) => {
+        if (windowTop > element.offsetTop) {
+            element.classList.add(animationClass);
+        }
+    });
+}
+
+animeScroll();
+
+if (target.length) {
+    window.addEventListener('scroll', debounce(() => {
+        animeScroll();
+    }, 100));
+}
